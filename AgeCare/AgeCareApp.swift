68fae155f98d7a8721401e69bfd2b0cf18d34ea8
Felix Wei <felix.weissleder@tum.de>
2025-11-21 22:39:40 +0100

    Initial Commit

---
+++ AgeCare/AgeCareApp.swift
@@ -0,0 +1,32 @@
+//
+//  AgeCareApp.swift
+//  AgeCare
+//
+//  Created by Felix Weißleder on 21.11.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AgeCareApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
