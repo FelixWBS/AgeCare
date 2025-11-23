1a076f1e69309e934da7ac55f78fe278c9018707
Felix Wei <felix.weissleder@tum.de>
2025-11-23 07:12:56 +0100

    Full Project

---
+++ AgeCareTests/AgeCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgeCareTests.swift
+//  AgeCareTests
+//
+//  Created by Felix Weißleder on 21.11.25.
+//
+
+import Testing
+@testable import AgeCare
+
+struct AgeCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
